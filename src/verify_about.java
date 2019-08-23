835bc06a870fdc3bc24df894288d84275c59f559
deepa <deepa@LAPTOP-6RHKGB6P>
2019-08-23 16:11:34 +0530

    Firstcommit

---
+++ src/verify_about.java
@@ -0,0 +1,33 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+public class verify_about {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://ec2-3-17-109-75.us-east-2.compute.amazonaws.com/index.php");
+		String x=driver.getTitle();
+		System.out.println(x);
+		String z = null;
+		try { driver.findElementById("About Us").click();
+		z =driver.findElementById("PID-ab2-pg").getText();
+		System.out.println(z);
+		}
+		catch (Exception e) {
+			System.out.println("Invalid Element");
+			z="";
+		}
+		
+		int a=z.length();
+		System.out.println(a);
+		if (z.length() > 0)  {
+			System.out.println("Success");
+			}
+		else 
+		{
+			System.out.println("Failure");	
+		}
+	}
+
+}
